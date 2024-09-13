a89d4a452182d6e8f557cb0c1fa7a922680f9d02
Wesley Rodrigues <ley.desenvolvedor@gmail.com>
2024-09-13 14:25:04 -0300

    Iniciando projeto com Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+        fontFamily: {
+          graphik: ['Graphik', 'sans-serif'],
+          'graphik-bold':['GraphikBold', 'sans-serif']
+        },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
