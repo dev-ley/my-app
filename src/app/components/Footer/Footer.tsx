bec1b9f1f6abe561d0ab14bb2da22f6569ac05ef
Wesley Rodrigues <ley.desenvolvedor@gmail.com>
2024-10-01 15:25:08 -0300

    finalizado projeto

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,15 +4,15 @@ import Image from 'next/image'
 
 const Footer = () => {
     return (
-        <footer className='bg-[#252B42]  text-white h-[1431px] flex flex-col items-center py-[107px]'>
-            <div className=' w-[284px] h-[1217px] flex flex-col items-center gap-[76px]'>
-                <div className=' w-[186px] h-[947px] flex flex-col items-center gap-[73px]'>
+        <footer className='bg-[#252B42]  text-white h-[1431px] flex flex-col items-center py-[107px] lg:h-[567px] lg:flex-col lg:items-center lg:justify-center'>
+            <div className=' w-[284px] h-[1217px] flex flex-col items-center gap-[76px] lg:w-[991px] lg:h-[267px] lg:flex-row lg:items-start lg:gap-[125px]'>
+                <div className=' w-[186px] h-[947px] flex flex-col items-center gap-[73px] lg:flex-row lg:w-[582px] lg:h-[267px]'>
                     <FooterCard />
                     <FooterCard />
                     <FooterCard />
                 </div>
 
-                <div className=' w-[284px] h-[194px] flex flex-col items-center gap-[10px]'>
+                <div className=' w-[284px] h-[194px] flex flex-col items-center gap-[10px] lg:items-start'>
                     <div className=' flex w-[284px] h-[46px] items-center' >
                         <Image src={'/images/footer/bx_bx-map.svg'} alt='map' width={46} height={46} />
                         <p className='text-[15px]'>7480 Mockingbird Hill undefined </p>
--- src/app/components/Footer/FooterCard.tsx
+++ src/app/components/Footer/FooterCard.tsx
@@ -3,11 +3,11 @@ import H3 from '../Texto/H3'
 
 const FooterCard = () => {
     return (
-        <div className='w-[186px] h-[267px] flex flex-col items-center gap-[15px]'>
+        <div className='w-[186px] h-[267px] flex flex-col items-center gap-[15px] lg:items-start '>
             <div className='w-[167px] h-[48px] flex flex-col items-center py-[10px] gap-[10px]'>
                 <H3 text='Annette Steward' />
             </div>
-            <div className='w-[122px] h-[204px] flex flex-col items-center justify-between gap-[16px]'>
+            <div className='w-[122px] h-[204px] flex flex-col items-center justify-between gap-[16px] lg:items-start lg:justify-start '>
                 <a className='text-[15px]' href="#">Jennie Cooper</a>
                 <a className='text-[15px]' href="#">Julie Henry</a>
                 <a className='text-[15px]' href="#">Johnny Murphy</a>
